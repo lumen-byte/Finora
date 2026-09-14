834f578259f6c7bb5bdfd440cc029cc9efb4e411
Abhimanyu <abhimanyu2004111@gmail.com>
2026-09-14 14:01:10 +0530

    fix: allow all cors origins by default to support vercel

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,14 +9,13 @@ app = FastAPI(
 )
 
 # Set all CORS enabled origins
-if settings.CORS_ORIGINS:
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=[str(origin) for origin in settings.CORS_ORIGINS],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[str(origin) for origin in settings.CORS_ORIGINS] if settings.CORS_ORIGINS else ["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router, prefix="/api/v1")
 
